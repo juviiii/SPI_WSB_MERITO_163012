d1f32896d6640dcb623f4fbbdb8cb60985b3f817
juviiii <hubertbogu@gmail.com>
2024-06-30 20:19:06 +0200

    Plik tworzący login dla użytkownika po zadaniu dwóch pytań

---
+++ plik_1.py
@@ -0,0 +1,16 @@
+from string import ascii_letters
+
+
+def get_name():
+    name = input("What is your name? ").strip().title()
+    while not all(letter in ascii_letters + " -" for letter in name):
+        name = input("Please enter a valid name: ").strip().title()
+    return name
+
+def main():
+    name = get_name()
+    age = input("What is your age? ")
+    print(f"Hi, my name is {name}, and I am {age} years old.")
+
+if __name__ == "__main__":
+    main()
