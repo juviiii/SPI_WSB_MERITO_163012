e6256d4b7a9c04bdfa11e4a43a4c7000672a7d3a
juviiii <hubertbogu@gmail.com>
2024-06-30 20:25:05 +0200

    Doodanie pytania o nazwisko dla zwiększenia unikatowości

--- plik_1.py
+++ plik_1.py
@@ -7,11 +7,18 @@ def get_name():
         name = input("Please enter a valid name: ").strip().title()
     return name
 
+def get_surname():
+    surname = input("What is your last name? ").strip().title()
+    while not all(letter in ascii_letters + " -" for letter in surname):
+        surname = input("Please enter a valid last name: ").strip().title()
+    return surname
+
 def main():
     name = get_name()
+    surname = get_surname()
     age = input("What is your age? ")
     #print(f"Hi, my name is {name}, and I am {age} years old.")
-    print(f"Your new login is: {name}_{age}")
+    print(f"Your new login is: {name}_{age}_{surname}")
 
 if __name__ == "__main__":
     main()
