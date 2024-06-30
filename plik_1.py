d15259d65bcc33f63877548e823097f7a5b994b1
juviiii <hubertbogu@gmail.com>
2024-06-30 20:22:28 +0200

    Poprawiony output przy print - wcześniej zwracał zdanie o imieniu i wieku zamiast loginu

--- plik_1.py
+++ plik_1.py
@@ -10,7 +10,8 @@ def get_name():
 def main():
     name = get_name()
     age = input("What is your age? ")
-    print(f"Hi, my name is {name}, and I am {age} years old.")
+    #print(f"Hi, my name is {name}, and I am {age} years old.")
+    print(f"Your new login is: {name}_{age}")
 
 if __name__ == "__main__":
     main()
